021e967c04cd3239678ab15c5d1abd8047e94e18
Aanchal <aanchalverma.av@outlook.com>
2025-07-25 14:00:02 +0530

    Added reported and created yml for github actions

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: true,
+      html: true,
+      json: true
+    }
   },
 });
